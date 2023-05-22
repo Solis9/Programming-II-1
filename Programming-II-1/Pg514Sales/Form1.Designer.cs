9605bccc42bde1717f71201fa5dcfa9b3cdbbc63
Solis9 <solis.y@stu.janesville.k12.wi.us>
2023-05-22 08:21:46 -0500

    update

--- Programming-II-1/Pg514Sales/Form1.Designer.cs
+++ Programming-II-1/Pg514Sales/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.lblLowest = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lblHighest = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -45,12 +45,12 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.lblLowest);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblHighest);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblAverage);
+            this.groupBox1.Controls.Add(this.lblTotal);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
@@ -68,14 +68,14 @@
             this.label7.TabIndex = 7;
             this.label7.Text = "Lowest Sales:";
             // 
-            // label8
+            // lblLowest
             // 
-            this.label8.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label8.Location = new System.Drawing.Point(181, 187);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(90, 22);
-            this.label8.TabIndex = 6;
+            this.lblLowest.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblLowest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLowest.Location = new System.Drawing.Point(181, 187);
+            this.lblLowest.Name = "lblLowest";
+            this.lblLowest.Size = new System.Drawing.Size(90, 22);
+            this.lblLowest.TabIndex = 6;
             // 
             // label5
             // 
@@ -85,14 +85,14 @@
             this.label5.TabIndex = 5;
             this.label5.Text = "Highest Sales:";
             // 
-            // label6
+            // lblHighest
             // 
-            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label6.Location = new System.Drawing.Point(181, 138);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(90, 22);
-            this.label6.TabIndex = 4;
+            this.lblHighest.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblHighest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHighest.Location = new System.Drawing.Point(181, 138);
+            this.lblHighest.Name = "lblHighest";
+            this.lblHighest.Size = new System.Drawing.Size(90, 22);
+            this.lblHighest.TabIndex = 4;
             // 
             // label3
             // 
@@ -102,23 +102,23 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Average Sales:";
             // 
-            // label4
+            // lblAverage
             // 
-            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label4.Location = new System.Drawing.Point(181, 89);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 22);
-            this.label4.TabIndex = 2;
+            this.lblAverage.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAverage.Location = new System.Drawing.Point(181, 89);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(90, 22);
+            this.lblAverage.TabIndex = 2;
             // 
-            // label2
+            // lblTotal
             // 
-            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(181, 40);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 22);
-            this.label2.TabIndex = 1;
+            this.lblTotal.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal.Location = new System.Drawing.Point(181, 40);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(90, 22);
+            this.lblTotal.TabIndex = 1;
             // 
             // label1
             // 
@@ -153,7 +153,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(554, 423);
+            this.ClientSize = new System.Drawing.Size(328, 305);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
@@ -168,12 +168,12 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblLowest;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblHighest;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- Programming-II-1/Pg514Sales/Form1.cs
+++ Programming-II-1/Pg514Sales/Form1.cs
@@ -18,8 +18,7 @@ namespace Pg514Sales
             InitializeComponent();
         }
 
-        private bool GetSalesData(ref decimal[] decSales)
-        {
+        private bool GetSalesData(ref decimal[] decSales) {
             decimal[] decSalesData;
             int intNumDays = 0;
             int intCount;
@@ -36,13 +35,83 @@ namespace Pg514Sales
                 decSalesData = new decimal[intNumDays];
                 for (intCount = 0; intCount < intNumDays; intCount++) {
                     bool blnValid = false;
+                    while (blnValid != true) {
+                        blnValid = decimal.TryParse(Interaction.InputBox("Enter the sales " + "for day " + (intCount + 1).ToString()),
+                                                                        out decSalesData[intCount]);
+                        if (blnValid != true) {
+                            MessageBox.Show("Please enter a valid number");
+                        }
+                    }
+                }
+
+                    decSales = decSalesData;
+                    blnSuccess = true;
+                } else {
+                    MessageBox.Show("You must enter at least " + "oneday of sales");
+                }
+                return blnSuccess;
+            }
+
+        private decimal GetTotal(decimal[] decValues) {
+            decimal decTotal = 0;
+            int intCount;
+
+            for (intCount = 0; intCount < decValues.Length; intCount++) {
+                decTotal += decValues[intCount];
+            }
+            return decTotal;
+        }
+
+        private decimal GetAverage(decimal[] decValues) {
+            return GetTotal(decValues) / decValues.Length;
+        }
+
+        private decimal GetHighest(decimal[] decValues) {
+            int intCount;
+            decimal decHighest = decValues[0];
+
+            for (intCount = 1; intCount < decValues.Length; intCount++) {
+                if (decValues[intCount] > decHighest) {
+                    decHighest = decValues[intCount];
                 }
             }
+            return decHighest;
+        }
+
+        private decimal GetLowest(decimal[] decValues)
+        {
+            int intCount;
+            decimal decLowest = decValues[0];
+
+            for (intCount = 1; intCount < decValues.Length; intCount++)
+            {
+                if (decValues[intCount] > decLowest)
+                {
+                    decLowest = decValues[intCount];
+                }
+            }
+            return decLowest;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            decimal[] decSales = null;
+            decimal decTotal;
+            decimal decAverage;
+            decimal decHighest;
+            decimal decLowest;
 
+            if (GetSalesData (ref decSales)) {
+                decTotal = GetTotal(decSales);
+                decAverage = GetAverage(decSales);
+                decHighest = GetHighest(decSales);
+                decLowest = GetLowest(decSales);
+
+                lblTotal.Text = decTotal.ToString("$.00");
+                lblAverage.Text = decAverage.ToString("$.00");
+                lblHighest.Text = decHighest.ToString("$.00");
+                lblLowest.Text = decLowest.ToString("$.00");
+            }
         }
     }
 }
